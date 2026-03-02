4292f42a46e2ec7d57f4945766df01a7709ecd92
Richard Liebscher <r1tschy@posteo.de>
2026-03-02 21:05:54 +0100

    Add more reduce functions for slices

--- slicex/reduce.go
+++ slicex/reduce.go
@@ -18,3 +18,44 @@ func ReduceValues[V any, A any](slice []V, initial A, f func(A, V) A) A {
 	}
 	return acc
 }
+
+func Find[V any](slice []V, f func(V) bool) (V, bool) {
+	for _, v := range slice {
+		if f(v) {
+			return v, true
+		}
+	}
+	var zero V
+	return zero, false
+}
+
+func CountIf[V any](slice []V, pred func(int, V) bool) int {
+	return ReduceAll(slice, 0, func(acc int, idx int, value V) int {
+		if pred(idx, value) {
+			acc += 1
+		}
+		return acc
+	})
+}
+
+func Any[V any](slice []V, pred func(int, V) bool) bool {
+	for idx, val := range slice {
+		if pred(idx, val) {
+			return true
+		}
+	}
+	return false
+}
+
+func All[V any](slice []V, pred func(int, V) bool) bool {
+	for idx, val := range slice {
+		if !pred(idx, val) {
+			return false
+		}
+	}
+	return true
+}
+
+func None[V any](slice []V, pred func(int, V) bool) bool {
+	return !Any(slice, pred)
+}
